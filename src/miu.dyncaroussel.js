1c9970ead5b3e89910dccd6c2d4b96eedf0afbe2
Zekeng Ndadji Milliam Maxime <ndadjimaxime@yahoo.fr>
2018-11-17 07:52:25 +0100

    Basic structure

---
+++ src/miu.dyncaroussel.js
@@ -0,0 +1,8 @@
+/**
+ *	JS Processor for MIU Grid Component
+**/
+function miuDynCaroussel () {
+	if(typeof miuDomLib === 'undefined') {
+		throw new Error('MIU DOM Library is required for MIU Dynamise Caroussel')
+	}
+}
